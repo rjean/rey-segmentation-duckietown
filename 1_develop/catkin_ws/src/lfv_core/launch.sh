f63cad18aab7926d0c90a0412eccfe2dbb576098
rrwiyatn <rey.reza@hotmail.com>
2019-12-30 13:33:10 -0500

    add lfv_core

---
+++ 1_develop/catkin_ws/src/lfv_core/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+echo "This is an empty launch script. Update it to launch your application."
+roslaunch rey_pure_pursuit rey_pure_pursuit.launch
